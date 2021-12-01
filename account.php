535ef59a3e48ccb0fd8da260228837488bd078aa
Adrien <adrien.bouteiller01@gmail.com>
2021-12-01 15:15:45 +0100

    index finish

---
+++ account.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Account</title>
+</head>
+<body>
+<div class="container">
+    <label for="name">My account</label>
+    <input type="text" id="name" name="name" required
+           minlength="4" maxlength="8" size="10">
+</div>
+</body>
+</html>
