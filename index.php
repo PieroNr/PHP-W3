df3677a7376e05f5cc682a5b986f7dcf39f9baa6
Adrien <adrien.bouteiller01@gmail.com>
2021-12-01 14:40:02 +0100

    wip index

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Accueil</title>
-    </head>
-    <body>
-        <div class="container" style="font-family: Arial, sans-serif;">
-            <div class="post" style="background-color: grey; width: 500px">
-                <h2><strong>Exemple post title</strong></h2>
-                <p>Ecrit par : "nom de la personne" <br/> Le : jj/mm/aaaa à hh:min</p>
-                <img alt="" src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80" style="width: 100%"/>
-                <p>Description du texte</p>
-                <div class="commentaire">
-                    <h3>Les commentaires</h3>
-                    <textarea name="commentaire" id="input1" class="inputTextArea" rows="4" cols="48"></textarea>
-                </div>
-            </div>
-        </div>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="src/assets/css/app.css">
+    <link rel="stylesheet" href="src/assets/css/post.css">
+</head>
+<body>
+<div class="container">
+    <div class="post">
+        <h2><strong>Exemple post title</strong></h2>
+        <p>Ecrit par : "nom de la personne" <br/> Le : jj/mm/aaaa à hh:min</p>
+        <img alt=""
+             src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"/>
+        <p>Description du texte</p>
+        <h3>Les commentaires</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, beatae, neque? Amet asperiores at cum
+            earum eos est itaque magnam, molestiae odit quod reiciendis repellendus sapiente, tempora totam ut,
+            voluptas!</p>
+    </div>
+    <div class="addcomments">
+        <h3>Ajouter un commentaire</h3>
+        <textarea name="commentaire" id="input1" class="inputTextArea" rows="4" cols="48"></textarea>
+    </div>
+</div>
+</body>
 </html>
