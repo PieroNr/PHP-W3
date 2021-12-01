c1bd404187b07ac4608de3f23e49bc0120471cc4
Adrien <adrien.bouteiller01@gmail.com>
2021-12-01 15:35:56 +0100

    my account current modification

--- account.php
+++ account.php
@@ -5,13 +5,39 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="src/assets/css/app.css">
+    <link rel="stylesheet" href="src/assets/css/form.css">
     <title>Account</title>
 </head>
 <body>
 <div class="container">
-    <label for="name">My account</label>
-    <input type="text" id="name" name="name" required
-           minlength="4" maxlength="8" size="10">
+    <h1>My account</h1>
+    <form action="" method="get" class="form-example">
+        <div class="form">
+            <label for="name">Nom: </label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div class="form">
+            <label for="name">Prénom: </label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div class="form">
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div class="form">
+            <label for="password">Mot de passe : </label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div class="form">
+            <label for="password">Vérifier le mot de passe : </label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div class="form">
+            <input class="btn" type="submit" value="Modifier">
+        </div>
+    </form>
+
 </div>
 </body>
 </html>
