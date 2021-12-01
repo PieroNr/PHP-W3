031b5c81a59242a0959e35031f6313f10ae54e50
Tristan Seclet <tristanseclet@gmail.com>
2021-12-01 13:59:57 +0100

    initial commit adrien//tristan

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Accueil</title>
+    </head>
+    <body>
+        <div class="container" style="font-family: Arial, sans-serif;">
+            <div class="post" style="background-color: grey; width: 500px">
+                <h2><strong>Exemple post title</strong></h2>
+                <p>Ecrit par : "nom de la personne" <br/> Le : jj/mm/aaaa à hh:min</p>
+                <img alt="" src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80" style="width: 100%"/>
+                <p>Description du texte</p>
+                <div class="commentaire">
+                    <h3>Les commentaires</h3>
+                    <textarea name="commentaire" id="input1" class="inputTextArea" rows="4" cols="48"></textarea>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
